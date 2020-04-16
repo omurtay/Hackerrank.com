4fc4ec0641334306f8be1e1ecf248451c7332b69
omurtay <omurtay94@gmail.com>
2020-04-16 15:06:00 +0300

    Update List Comprehensions.py

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -1,16 +1,10 @@
-if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
 
-ar = [] 
-p = 0 
-for i in range ( x + 1 ): 
-    for j in range( y + 1): 
-        for k in range(z+1):
-            if i+j+k != n:
-                ar.append([]) 
-                ar[p] = [ i , j,k ] 
-                p+=1 
-print(ar)
+
+def solveMeFirst(a,b):
+    return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
